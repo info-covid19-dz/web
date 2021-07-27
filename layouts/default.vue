ad6ee4bfd9d67cf7d8681a430ec2354723e63910
nezlicodes <benmeradi.lilyanezli@gmail.com>
2021-07-27 21:24:25 +0100

    responsive navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,49 @@
 <template>
   <v-app dark>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-      color="primary"
-      dark
-    >
-      <nuxt-link class="dark-link"  to="/">{{title}}</nuxt-link>
+    <v-app-bar :clipped-left="clipped" fixed app color="primary" dark>
+      <nuxt-link class="dark-link" to="/">{{ title }}</nuxt-link>
       <v-spacer />
-   
-      <nuxt-link class="dark-link" to="/annuaire">Annuaire</nuxt-link>
+      <v-toolbar-items
+        v-for="(item, i) in items"
+        :key="i"
+        class="hidden-sm-and-down justify-end"
+      >
+        <v-btn text >
+          <nuxtLink  class="dark-link" :to="item.link">{{ item.title }}</nuxtLink></v-btn
+        >
+      </v-toolbar-items>
+
+      <v-layout class="justify-end hidden-md-and-up">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn large icon color="#060606" dark v-on="on">
+              <v-icon large> mdi-menu</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, i) in items" :key="i + item.id">
+              <v-list-item-title
+                ><nuxt-link :to="item.link">{{
+                  item.title
+                }}</nuxt-link></v-list-item-title
+              >
+            </v-list-item>
+          
+          </v-list>
+        </v-menu>
+      </v-layout>
+
+      <!--  <nuxt-link class="dark-link" to="/annuaire">Annuaire</nuxt-link>
       <nuxt-link class="dark-link" to="/cagnottes">Cagnottes</nuxt-link>
-      <nuxt-link class="dark-link" to="/contact">Contact</nuxt-link>
+      <nuxt-link class="dark-link" to="/tests">Tests</nuxt-link>
+      <nuxt-link class="dark-link" to="/contact">Contact</nuxt-link> -->
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -30,37 +51,43 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          title: "Annuaire",
+          link: "/annuaire",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          title: "Tests",
+          link: "/tests",
+        },
+        {
+          title: "Cagnottes",
+          link: "/cagnottes",
+        },
+        {
+          title: "Contact",
+          link: "/contact",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Annuaire COVID Algerie'
-    }
-  }
-}
+      title: "Annuaire COVID Algerie",
+    };
+  },
+};
 </script>
 
 
 <style>
-  .dark-link{
-    padding-inline: 0.5em;
-    text-decoration: none;
-    color:white !important
-  }
+.dark-link {
+  padding-inline: 0.5em;
+  text-decoration: none;
+  color: white !important;
+}
 </style>
