87a2d253bcd4ae1b9e083c066e735728fb0e6218
nezlicodes <benmeradi.lilyanezli@gmail.com>
2021-07-28 08:02:17 +0100

    updated the data table

--- pages/Annuaire.vue
+++ pages/Annuaire.vue
@@ -1,15 +1,54 @@
 <template>
-    <div>
-
-    </div>
+  <v-data-table
+    :headers="headers"
+    :items="annuaires"
+    :items-per-page="15"
+    class="elevation-1"
+    :search="search"
+  >
+    <template v-slot:item.Vérifié="{ item }">
+      <v-chip :color="getColor(item.Vérifié)" dark>
+        {{ item.Vérifié }}
+      </v-chip>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
-    export default {
-        
-    }
+import annuaire from "../static/annuaire/annuaire.json";
+export default {
+  data() {
+    return {
+      search: "",
+      headers: [
+        {
+          text: "Entreprise",
+          align: "start",
+          sortable: false,
+          value: "Nom",
+        },
+        { text: "Wilaya", value: "Wilaya", sortable: true },
+        { text: "Addresse", value: "Addresse" },
+        { text: "Numero", value: "Numero" },
+        { text: "Vérifié", value: "Vérifié", sortable: true },
+        { text: "Type", value: "Type" },
+        { text: "Service", value: "Service" },
+      ],
+      annuaires: annuaire,
+    };
+  },
+  methods: {
+    getColor(verified) {
+      if (verified) {
+        return "green";
+      } else {
+        return "red";
+      }
+    },
+  },
+  mounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>
