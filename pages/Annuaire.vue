ba5cb3a4d913c881b47ebc00f24bfb81a8f84bb6
nezlicodes <benmeradi.lilyanezli@gmail.com>
2021-07-28 09:52:35 +0100

    adding annuaire datatable

--- pages/Annuaire.vue
+++ pages/Annuaire.vue
@@ -1,17 +1,32 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="annuaires"
-    :items-per-page="15"
-    class="elevation-1"
-    :search="search"
-  >
-    <template v-slot:item.Vérifié="{ item }">
-      <v-chip :color="getColor(item.Vérifié)" dark>
-        {{ item.Vérifié }}
-      </v-chip>
-    </template>
-  </v-data-table>
+  <div class="mt-3">
+    <h1 class="mb-5">
+      Retrouvez tous les numéros concernant la vente et location de
+      concentrateurs d'oxygène
+    </h1>
+    <v-btn class="mb-5" color="primary">
+      <a class="dark-link"
+        target="_blank"
+        href="https://docs.google.com/spreadsheets/d/1a4N_yGTUOmGVFRLODbfv6NNJO7hcgdXpayRFSufj1sk/edit?usp=sharing"
+      >
+        Ajouter un numéro</a
+      ></v-btn
+    >
+    <v-data-table
+      color="#470237"
+      :headers="headers"
+      :items="annuaires"
+      :items-per-page="15"
+      class="elevation-1"
+      :search="search"
+    >
+      <template v-slot:item.Vérifié="{ item }">
+        <v-chip :color="getColor(item.Vérifié)" dark>
+          {{ item.Vérifié }}
+        </v-chip>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
